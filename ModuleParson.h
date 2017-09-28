cdc888a9e3c40996bdb7fe2c38b45165f0af7276
Pau Olivé <rcolivepau@gmail.com>
2017-09-28 13:39:18 +0200

    Parson Module

--- ModuleParson.h
+++ ModuleParson.h
@@ -4,6 +4,31 @@
 #define __MODULE_PARSON_H__
 
 #include "Module.h"
+#include "parson.h"
+
+class JSON_Doc
+{
+public:
+	JSON_Doc(JSON_Value* value, JSON_Object* object, const char* path);
+	~JSON_Doc();
+
+	void SetString(const char* set, const char* str);
+	void SetBool(const char* set, bool bo);
+	void SetNumber(const char* set, double nu);
+	const char* GetString(const char* str);
+	bool GetBool(const char* bo);
+	double GetNumber(const char* nu);
+
+	const char* GetPath();
+	void Save();
+	void CleanUp();
+
+private:
+	JSON_Value*  value = nullptr;
+	JSON_Object* object = nullptr;
+	const char*  path;
+};
+
 
 class ModuleParson : public Module {
 public:
@@ -12,6 +37,12 @@ public:
 
 	bool Init();
 	bool CleanUp();
+
+	JSON_Doc* LoadJSON(const char* path);
+	JSON_Doc* CreateJSON(const char* path);
+
+private:
+	std::list<JSON_Doc*> jsons;
 };
 
 #endif // !__MODULE_PARSON_H__
